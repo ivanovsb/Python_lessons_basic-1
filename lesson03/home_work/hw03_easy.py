4c3e0d0337a1bd6f77efdbb904ec4d2b6403abda
Sergey Ivanov <ivanovsb@mail.ru>
2018-09-13 19:11:12 +0500

    Удалил из основной ветки изменения Домашнее задание №3_easy, чтобы перенести работу в ветку Lesson03

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -14,30 +14,6 @@ print(my_round(2.1234567, 5))
 print(my_round(2.1999967, 5))
 print(my_round(2.9999967, 5))
 
-def my_round(a,b):
-    lst=str(a).split('.')
-    a1 = lst[0]
-    a2 = lst[1]
-    print('Число: ' + str(a) + ' округляем до ' + str(b) + ' знаков после запятой')
-    if (int(a2[b])) < 5:
-        #Удаляем лишние нули справа
-        res = del_zero(a2[0:b])
-        res = a1 +'.' + str(res)
-    else:
-        res = int(a2[0:b]) + 1
-        if len(str(res)) > b:
-            res = int(a1)+1
-        else:
-            res = del_zero(res)
-            res = a1 +'.' + str(res)
-    return(str(res))
-
-def del_zero(item):
-    while not (int(item) % 10):
-        item = int(item) // 10
-    return item
-    
-print('Результат: ', my_round(2.9999967, 5))
 
 
 # Задание-2:
@@ -54,18 +30,4 @@ print(lucky_ticket(123006))
 print(lucky_ticket(123213))
 print(lucky_ticket(436751))
 
-def lucky_ticket(bilet):
-    sum1 = summa(str(bilet)[0:3])
-    sum2 = summa(str(bilet)[3:])
-    if sum1 == sum2:
-        return 'Счастливый'
-    else:
-        return 'Не счастливый'
 
-def summa(my_string):
-    my_sum = 0
-    for i in range(len(my_string)):
-        my_sum+=int(my_string[i])
-    return my_sum
-
-print(lucky_ticket(123600))
