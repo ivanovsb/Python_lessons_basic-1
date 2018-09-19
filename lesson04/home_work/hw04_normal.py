c39d4bd236adb89e55e18833c4b6a164820f2fe7
Ivanov Sergey <ivanovsb@mail.ru>
2018-09-19 22:57:31 +0500

    Домашнее задание №4normal. Добавлены задания 2 и 3.

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -1,4 +1,4 @@
-# Задание-1:
+﻿# Задание-1:
 # Вывести символы в нижнем регистре, которые находятся вокруг
 # 1 или более символов в верхнем регистре.
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
@@ -58,9 +58,64 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+pattern = r'[a-z]{2}([A-Z]+)[A-Z]{2}'
+print(re.findall(pattern,line_2))
+
+Без рекурсии:
+temp_list =[]
+lst = line_2       
+len_string = len(lst)
+i = 0
+a = 0
+while i < (len_string-5):
+    if lst[i].islower() and lst[i+1].islower():
+        a = i+2 
+        try:
+            while (lst[a].isupper()):
+                a += 1
+        except:
+            print('перелет')
+        if (a-i)>=5:
+            temp_list.append(lst[i+2:a-2])
+            i = a
+        else:
+            i += 1
+    else:
+        i += 1
+print(temp_list)
+
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+import re, random
+sp = 2500
+lst = ''
+for i in range(sp):
+    num = random.randint(0,9)
+    lst+=str(num)
+f = open('hw04_text.txt','w')
+f.write(lst)
+f.close()
+
+with open('hw04_text.txt','r') as f:
+    lst2 = f.readlines()
+pattern = r'(.)\1+'
+
+   
+def smax(f_pattern, f_line):
+    cnt = 0
+    result = ''
+    for el in re.finditer(f_pattern,f_line):
+        if cnt < len(el.group(0)):
+            result = el.group(0)
+            cnt = len(el.group(0))
+    return result
+
+#Для проверки работы с файлами выводим два результата, которые должны быть одинаковыми
+print('Результат 1: ',smax(pattern, lst))
+print('Результат 2: ',smax(pattern, str(lst2)))
