4b62d17638face16eab99dda6f4b58dbc3deffe6
Sergey Ivanov <ivanovsb@mail.ru>
2018-09-20 17:51:22 +0500

    Домашнее задание №5_easy

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -2,9 +2,26 @@
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
+import os
+for i in range(1,10):
+dir_name = 'dir_{0}'.format(str(i))
+os.mkdir(dir_name)
+
+for i in range(1,10):
+dir_name = 'dir_{0}'.format(str(i))
+os.rmdir(dir_name)
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
+import os
+thedir = os.getcwd()
+d = [name for name in os.listdir(thedir) if os.path.isdir(os.path.join(thedir, name))]
+print(d)
+
+
+
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+shutil.copy(r'hw05_easy.py', r'hw05_easy_dupl.py')
